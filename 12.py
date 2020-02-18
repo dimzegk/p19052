067c87783dc9769a008b36b0a76026074f485160
dimzegk <61157931+dimzegk@users.noreply.github.com>
2020-02-18 15:34:21 +0200

    Update 12.py

--- 12.py
+++ 12.py
@@ -6,9 +6,7 @@ year=int(date[2])
 for i in range (0,len(date)):
     date[i]=int(date[i])
 date=datetime.date(date[2],date[1],date[0])
-print date
 now=datetime.date.today()
-print now
 diff=abs(date-now)
 print "Οι δύο ημερομηνίες απέχουν",diff.days," μέρες ή",(diff.days)*24,"ώρες ή",(diff.days)*24*3600,"δευτερόλεπτα."
 from calendar import monthrange
